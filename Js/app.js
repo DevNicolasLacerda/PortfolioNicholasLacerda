d49e111bb533850ab6d9c975bbc1879fccf96c47
DevNicolasLacerda <nicklacerda2018@gmail.com>
2023-05-08 00:44:18 -0300

    Firt commit

---
+++ Js/app.js
@@ -0,0 +1,46 @@
+const voltar_topo_btn = document.querySelector("#voltar_topo_btn");
+
+window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 100) {
+            voltar_topo_btn.classList.add("active");
+            voltar_topo_btn.addEventListener("click", function () {
+                  //$("html, body").animate({ scrollTop: 0 }, "slow");
+                  window.scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: "smooth",
+                  });
+            });
+      } else {
+            voltar_topo_btn.classList.remove("active");
+      }
+});
+
+function menuShow() {
+      let menuMobile = document.querySelector(".mobile-menu");
+      if (menuMobile.classList.contains("open")) {
+            menuMobile.classList.remove("open");
+            document.querySelector(".icon").src = "assets/menu_white_36dp.svg";
+      } else {
+            menuMobile.classList.add("open");
+            document.querySelector(".icon").src = "assets/close_white_36dp.svg";
+      }
+}
+
+/*
+
+const switcher = document.querySelector(".btn_dark");
+
+switcher.addEventListener("click", function () {
+      document.body.classList.toggle("light-theme");
+      document.body.classList.toggle("dark-theme");
+
+      const className = document.body.className;
+      if (className == "light-theme") {
+            this.textContent = "Dark";
+      } else {
+            this.textContent = "Light";
+      }
+});
+
+*/
