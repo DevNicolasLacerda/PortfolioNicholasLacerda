40073eff84bf0017ca26fef4d88b67a3073c5552
Nicholas Lemes Bahia Lacerda <124929174+DevNicolasLacerda@users.noreply.github.com>
2023-05-10 16:36:17 -0300

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -53,20 +53,5 @@ text.textContent = "Developer";
 textLoad();
 setInterval(textLoad, 12000);
 
-/*
-
-const switcher = document.querySelector(".btn_dark");
-
-switcher.addEventListener("click", function () {
-      document.body.classList.toggle("light-theme");
-      document.body.classList.toggle("dark-theme");
-
-      const className = document.body.className;
-      if (className == "light-theme") {
-            this.textContent = "Dark";
-      } else {
-            this.textContent = "Light";
-      }
-});
-
 */
+
